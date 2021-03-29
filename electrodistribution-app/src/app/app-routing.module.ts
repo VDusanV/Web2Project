01aeb0feb1a254cb17d176b880223abd475e7425
VDusanV <VDusanV@users.noreply.github.com>
2021-03-29 19:33:55 +0200

    Add angular material
    Now we can add cool stuff from https://material.angular.io/

--- electrodistribution-app/src/app/app-routing.module.ts
+++ electrodistribution-app/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegistrationComponentComponent } from './registration-component/registration-component.component';
 
 const routes: Routes = [
-
+  {
+    path: "",
+    component: DashboardComponent
+  },
   {
     path: "registration",
     component: RegistrationComponentComponent
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent
   }
 
 
--- electrodistribution-app/src/app/app.module.ts
+++ electrodistribution-app/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
     NavBarComponent,
-      RegistrationComponentComponent
+      RegistrationComponentComponent,
+      DashboardComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
