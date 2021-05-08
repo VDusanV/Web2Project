727302f0efd4163f462e98664f54e9dc3c225df8
Minja Peričić <42515301+mpericic@users.noreply.github.com>
2021-05-09 00:26:14 +0200

    Add User DataBase Model

---
+++ Web2Backend/Web2Backend/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web2Backend.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string NameAndLastname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public byte[] ImageData { get; set; }
+        
+
+
+    }
+}
