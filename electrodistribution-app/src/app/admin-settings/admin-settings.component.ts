b785812ce283279afa5dbcf4de04bf47d1c9a692
Branislav Dobrokes <bdobrokes@gmail.com>
2021-06-09 23:51:16 +0200

    Admin Settings Update

--- electrodistribution-app/src/app/admin-settings/admin-settings.component.ts
+++ electrodistribution-app/src/app/admin-settings/admin-settings.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserService } from '../services/user/user.service';
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
+import { JsonObjectExpression } from 'typescript';
 
 @Component({
   selector: 'app-admin-settings',
@@ -7,7 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSettingsComponent implements OnInit {
 
-  constructor() { }
+  minimumCharacters:boolean = false;
+  invalidPassword:boolean = false;
+
+  constructor(private router:Router, private http:HttpClient, private userService:UserService) { }
+
+  changePassword(form:NgForm){
+    var form1 = form;
+    if(this.validate(form)){
+    const credentials = JSON.stringify(form1.value);
+    this.userService.changeUserPassword(credentials);
+    localStorage.clear();
+    this.router.navigate(["home"]);
+    }
+  }
+
+  validate(form:NgForm):boolean{
+    const credentials = JSON.stringify(form.value);
+    console.log('password: ' + credentials);
+    var password = JSON.parse(credentials);
+    password = password['password'].toString();
+    if(password.length < 6){  //validacija za duzinu sifre
+      this.minimumCharacters = true;
+      return false;
+    }
+    if(password.length != password.replace(/\s/g, "").length){  //validacija za razmake
+      this.invalidPassword = true;
+      return false;
+    }
+    return true;
+  }
 
   ngOnInit(): void {
   }
