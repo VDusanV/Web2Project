24030067950493de18345e8f981f3c4c92417d0e
Minja Peričić <42515301+mpericic@users.noreply.github.com>
2021-06-18 19:50:07 +0200

    Add Devices to Safety Document

--- electrodistribution-app/src/app/devices/devices.component.ts
+++ electrodistribution-app/src/app/devices/devices.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ElementsService } from '../services/elements.service';
 import { SharedService } from '../services/shared/shared.service';
+import { Element } from '../entities/element';
 
 @Component({
   selector: 'app-devices',
@@ -11,10 +13,16 @@ export class DevicesComponent implements OnInit {
   public component = "devices";
   public toNavbar = ["", this.component];
 
+  public allElements : Element[] = [];
+  public page = 10;
+  public pageSize = 3;
 
-  constructor(private router: Router, private _sharedService: SharedService) { }
+  constructor(private router: Router, private _sharedService: SharedService, private elementsService: ElementsService) { }
 
   ngOnInit(): void {
+    this.elementsService.loadElements()
+    .subscribe(data => this.allElements = data);
+
   }
 
 
